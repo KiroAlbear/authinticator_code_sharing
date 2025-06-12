6c92766f13a32601a11c5b7e33bd723a7bcf3d64
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-06-12 18:24:34 +0300

    removing unused code

--- lib/core/widgets/scanner_page.dart
+++ lib/core/widgets/scanner_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class ScannerPage extends StatelessWidget {
   const ScannerPage({super.key});
@@ -29,51 +28,6 @@ class ScannerPage extends StatelessWidget {
       ],
     );
   }
-
-  Future<bool> gethtmlpermissions() async {
-    // request web permissions for camera, microphone, gps, photolibrary, notifications
-// Requesting permissions for camera, microphone, gps, photolibrary, notifications
-
-    Map<Permission, PermissionStatus> statuses = await [
-      Permission.camera,
-    ].request();
-
-    // Checking if permissions are granted or not
-
-    if (statuses[Permission.camera] == PermissionStatus.granted) {
-      print('Camera permissions granted');
-      return true;
-    } else {
-      print('Camera permissions not granted');
-      return false;
-    }
-  }
-
-  //
-  // Future<bool> _requestCameraPermission() async {
-  //   // Check the current status
-  //   var status = await Permission.camera.status;
-  //
-  //   if (status.isGranted) {
-  //     return true;
-  //     // setState(() => _permissionStatus = 'Camera permission granted!');
-  //     // Proceed to use the camera
-  //   } else if (status.isDenied) {
-  //     var requestedStatus = await Permission.camera.request();
-  //     if (requestedStatus.isGranted) {
-  //       return true;
-  //       // setState(() => _permissionStatus = 'Camera permission granted!');
-  //     } else {
-  //       return false;
-  //       // setState(() => _permissionStatus = 'Camera permission denied.');
-  //     }
-  //   } else {
-  //     return false;
-  //     // setState(() {
-  //     //   _permissionStatus = 'Unknown permission state. Please try again.';
-  //     // });
-  //   }
-  // }
 
   String extractSecretKey(String otpAuthUrl) {
     final uri = Uri.parse(otpAuthUrl);
