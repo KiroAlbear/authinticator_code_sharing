28d79ac1259b3793ae29ed4db473034c025e2390
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-06-14 16:19:32 +0300

    adding base ref command

--- build_web/flutter_service_worker.js
+++ build_web/flutter_service_worker.js
@@ -37,14 +37,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3da0d0195946b9a95b782c51d740a0fa
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "396ef0c5da0af4a1a4b57431b17d16d1",
+"flutter_bootstrap.js": "4365b04b70e20302a2f17ead0d9e7240",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e773cd34c148c4de2b497cf595c0b567",
-"/": "e773cd34c148c4de2b497cf595c0b567",
-"main.dart.js": "1fc82acf59094cd4bdc0863729667bbf",
+"index.html": "92cbf4e11e39e07bf551cea442f1532b",
+"/": "92cbf4e11e39e07bf551cea442f1532b",
+"main.dart.js": "9bed3e2e78348f02de88a492d207e04e",
 "manifest.json": "aa58ef39fcdb971dacade3ef0d5319d8",
 "version.json": "229d308fd0e2935606e5b27ae1ff5ba2"};
 // The application shell files that are downloaded before a service worker can
