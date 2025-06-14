cd905d85b16c9ce822263f400043b2c558c81760
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-06-14 15:54:55 +0300

    changed logo

--- build_web/flutter_bootstrap.js
+++ build_web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "430391543"
+    serviceWorkerVersion: "1969717878"
   }
 });
