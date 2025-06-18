69c76bf47d73c4ea52ddff5fd0a770fc4f703317
Dokkan-Dev <158779921+Dokkan-Dev@users.noreply.github.com>
2025-06-18 17:15:11 +0300

    build web

--- build_web/flutter_bootstrap.js
+++ build_web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3958460133"
+    serviceWorkerVersion: "745129429"
   }
 });
